b97174bf27e60f1b305f9851ecc0fd50863946e4
MusaTamzid05 <tamzid101@gmail.com>
2020-05-18 20:55:48 +0600

    code refactored.

--- main.py
+++ main.py
@@ -16,7 +16,17 @@ class CameraDataCreator:
         self.time_diff = time_diff
         self.total_save = total_save
 
-    def run(self , size):
+    def run(self , video_src ,  size):
+
+        show_window = True
+
+        try:
+            video_src = int(video_src)
+        except ValueError:
+            # we dont show window for video.
+            show_window = False
+
+
 
         cap = cv2.VideoCapture(0)
         video_running = True
@@ -38,12 +48,14 @@ class CameraDataCreator:
                 index += 1
                 print("{} data saved".format(index))
 
-                if index == self.total_save:
+                if self._is_finished(index):
                     print("Total {} image saved.".format(self.total_save))
                     video_running = False
 
+            if show_window:
+                cv2.imshow("Image" , current_frame)
 
-            cv2.imshow("Image" , current_frame)
+
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 video_running = False
 
@@ -53,6 +65,9 @@ class CameraDataCreator:
     def _close(self , cap):
         cap.release()
         cv2.destroyAllWindows()
+
+    def _is_finished(self , frame_index):
+        return frame_index == self.total_save
 
 
     def _save(self , frame_index ,  image):
@@ -87,6 +102,7 @@ class CameraDataCreator:
 def main():
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("-v" , "--video" , default = "1", help = "Video Src")
     parser.add_argument("-d" , "--dst" , default = "Test", help = "Dst for image to save")
     parser.add_argument("-ts" , "--total_save" , type = int , default = 50 , help = "number of image to save")
     parser.add_argument("-s" , "--size" , type = int , default = 224 , help = "Image save size")
@@ -94,7 +110,7 @@ def main():
     args = parser.parse_args()
 
     camera = CameraDataCreator(args.dst  , total_save = args.total_save)
-    camera.run(size = (args.size , args.size))
+    camera.run(video_src = args.video , size = (args.size , args.size))
 
 
 if __name__ == "__main__":
